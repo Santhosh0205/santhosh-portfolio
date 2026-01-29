f71a277c84b6af0ee2aa0a519ad0f07e490331ed
Santhosh0205 <santhoshracer0205@gmail.com>
2026-01-29 07:51:03 +0530

    cors error admin fixed

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors({
     "https://www.santhoshkrish.space"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "x-admin-password"]
 }));
 app.use(express.json());
 
